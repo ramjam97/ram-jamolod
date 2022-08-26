3910747a64dd0300a1964fdab434e4611cb140b3
Ramjam97 <ram.jamolod@gmail.com>
2022-08-26 15:31:19 +0800

    added styling

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ramil Icalina Jamolod</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <style>
-        body{
-            background-color: grey;
-        }
-    </style>
 </head>
+
 <body>
-    <h1>Welcome!</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque nisi ex illo quaerat quo, esse repudiandae nemo dignissimos nobis quos excepturi sit tempora amet nulla necessitatibus quasi cum odio facilis?</p>
+    <div class="container">
+        <h1 class="text-primary">Welcome!</h1>
+        <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque nisi ex illo quaerat quo, esse repudiandae nemo dignissimos nobis quos excepturi sit tempora amet nulla necessitatibus quasi cum odio facilis?</p>
+    </div>
 </body>
+
 </html>
