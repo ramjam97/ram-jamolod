60b59d5b1d8e8c4f817a094542ecb2ea42979cd5
Ramjam97 <ram.jamolod@gmail.com>
2022-08-26 15:27:33 +0800

    added styling

--- index.php
+++ index.php
@@ -5,9 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ramil Icalina Jamolod</title>
+
+    <style>
+        body{
+            background-color: grey;
+        }
+    </style>
 </head>
 <body>
     <h1>Welcome!</h1>
-    <p>This is a sample</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque nisi ex illo quaerat quo, esse repudiandae nemo dignissimos nobis quos excepturi sit tempora amet nulla necessitatibus quasi cum odio facilis?</p>
 </body>
 </html>
