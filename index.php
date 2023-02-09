d7d7ed709a6fabfa72d8b27b72383a2cd37f0ec2
Ram Jam <60874051+ramjam97@users.noreply.github.com>
2023-02-09 15:10:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,14 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ramil Icalina Jamolod</title>
+    <title>Ramil Icalina Jamolod</title>https://github.com/ramjam97/ram-jamolod/blob/master/index.php
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
 </head>
 
 <body>
     <div class="container">
-        <h1 class="text-primary">Sample Page!</h1>
+        <h1 class="text-primary">Sample Page 123!</h1>
         <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque nisi ex illo quaerat quo, esse repudiandae nemo dignissimos nobis quos excepturi sit tempora amet nulla necessitatibus quasi cum odio facilis?</p>
     </div>
 </body>
